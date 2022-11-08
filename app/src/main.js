208943dc1d37dedaaef141d4386f6d74164bb41f
FelipeSoupinski <sssoupinski@gmail.com>
2022-11-08 00:26:12 -0400

    chore: cypress unit tests configuration.

--- app/src/main.js
+++ app/src/main.js
@@ -5,8 +5,9 @@ import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+Vue.use(router)
+Vue.use(vuetify)
+
 new Vue({
-  router,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
