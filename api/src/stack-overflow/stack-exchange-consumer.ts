cdfd5e7a09706fe2d048e1fdff8533fbb4dc1fb3
Soupinski <sssoupinski@gmail.com>
2023-04-04 12:17:08 -0400

    feat: use p-queue for concurrency control.

--- api/src/stack-overflow/stack-exchange-consumer.ts
+++ api/src/stack-overflow/stack-exchange-consumer.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Prisma } from '@prisma/client'
 import { Consumer, ResponseSO } from './protocols'
+import PQueue from 'p-queue'
 
 export class StackExchangeConsumer implements Consumer {
   constructor(
@@ -12,6 +13,8 @@ export class StackExchangeConsumer implements Consumer {
     endDate?: number
   ): Promise<ResponseSO<Prisma.QuestionCreateInput>> {
     try {
+      const queue = new PQueue({concurrency: 1})
+
       let queryUrl = `${process.env.SO_API_URL}/search/advanced?`
 
       if (startDate)
@@ -26,8 +29,8 @@ export class StackExchangeConsumer implements Consumer {
         + `&filter=!m()D0hGBUSIeCfI2Xo7TEcCTCLFnDz0kyM33*WmH4pJaCgs7hjd4.9df`
         + `&tagged=${this.tag}&run=true&key=${process.env.SO_CLIENT_KEY}`
 
-      const response = await axios.get(queryUrl)
-      return response.data
+      const response = await queue.add(async () => await axios.get(queryUrl))
+      return response?.data ?? response
     } catch (error) {
       console.error(error)
       throw new Error('Bad API request getQuestions')
