4b24e5669fdc2ac386dacc375ef870ae8e7e55f3
Soupinski <sssoupinski@gmail.com>
2023-04-04 11:32:19 -0400

    refactor: single responsability functions.

--- api/src/stack-overflow/queries/questions.ts
+++ api/src/stack-overflow/queries/questions.ts
@@ -1,5 +1,5 @@
 import { StackExchangeConsumer } from '../index'
-import { PrismaClient, Status } from '@prisma/client'
+import { PrismaClient, Prisma, Status, MetadataSO } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
@@ -16,32 +16,13 @@ export class QuestionsSOWorker {
   
     try {
       const api = new StackExchangeConsumer(this.tag)
-      const startExecutionTime = new Date().getTime()
   
-      const lastUpdatedMetadataByTag = await prisma.metadataSO.findFirst({
-        where: {
-          project_tag: this.tag,
-          status: Status.FINISHED,
-          last_question_time: {
-            not: null
-          }
-        },
-        orderBy: {
-          last_question_id: 'desc'
-        },
-      })
+      const lastUpdatedMetadataByTag = await this.getLastUpdatedMetadataByTag()
   
       if (lastUpdatedMetadataByTag?.last_question_time)
         startDate = (Number(lastUpdatedMetadataByTag.last_question_time) / 1000) + 1
   
-      const metadata = await prisma.metadataSO.create({
-        data: {
-          project_tag: this.tag,
-          mining_start_date: startDate ? new Date(startDate * 1000) : null,
-          mining_end_date: endDate ? new Date(endDate * 1000) : null,
-          execution_start_time: new Date(startExecutionTime),
-        }
-      })
+      const metadata = await this.createMetadata()
       metadataId = metadata.id
   
       let questions, lastQuestion
@@ -68,20 +49,7 @@ export class QuestionsSOWorker {
         iterations++
       } while (questions.has_more && questions.quota_remaining > 0)
   
-      if (lastQuestion?.creation_date) {
-        metadata.last_question_time = new Date(Number(lastQuestion.creation_date) * 1000) 
-      }
-      metadata.last_question_id = lastQuestion?.question_id ?? null
-      metadata.execution_end_time = new Date()
-      metadata.status = Status.FINISHED
-  
-      await prisma.metadataSO.update({
-        data: metadata,
-        where: {
-          id: metadata.id
-        }
-      })
-  
+      await this.updateMetadata(lastQuestion, metadata)
       await prisma.$disconnect()
       return true
   
@@ -98,6 +66,56 @@ export class QuestionsSOWorker {
       await prisma.$disconnect()
       return false
     }
+  }
+
+  async getLastUpdatedMetadataByTag() {
+    return await prisma.metadataSO.findFirst({
+      where: {
+        project_tag: this.tag,
+        status: Status.FINISHED,
+        last_question_time: {
+          not: null
+        }
+      },
+      orderBy: {
+        last_question_id: 'desc'
+      },
+    })
+  }
+
+  async createMetadata(
+    startDate?: number,
+    endDate?: number
+  ) {
+    const startExecutionTime = new Date().getTime()
+
+    return await prisma.metadataSO.create({
+      data: {
+        project_tag: this.tag,
+        mining_start_date: startDate ? new Date(startDate * 1000) : null,
+        mining_end_date: endDate ? new Date(endDate * 1000) : null,
+        execution_start_time: new Date(startExecutionTime),
+      }
+    })
+  }
+
+  async updateMetadata(
+    lastQuestion: Prisma.QuestionCreateInput | undefined,
+    metadata: MetadataSO
+  ) {
+    if (lastQuestion?.creation_date) {
+      metadata.last_question_time = new Date(Number(lastQuestion.creation_date) * 1000) 
+    }
+    metadata.last_question_id = lastQuestion?.question_id ?? null
+    metadata.execution_end_time = new Date()
+    metadata.status = Status.FINISHED
+
+    await prisma.metadataSO.update({
+      data: metadata,
+      where: {
+        id: metadata.id
+      }
+    })
   }
 
 } 
